7824f7ca1638b8b86c261390c0681554573617cb
nothingjlt <nothingjlt@gmail.com>
2021-02-20 22:33:37 +0200

    Prettify code
    Added option to evaluate test and validation sets together,
    since data set is small and large variation is seen in test results
    
    Added option to not evaluate accuracy in the evaluate function,
    to save time copying from GPU to CPU memory.
    Accuracy evaluation occurs every few epochs (defined as constant)
    
    Set reasonable hyper-parameters
    
    Added a few configurations of NNI tuners and the results of the
    batchTuner over multiple attempts of potential hyper-parameters

--- gcn_try.py
+++ gcn_try.py
@@ -30,6 +30,8 @@ DEFAULT_LABEL_TO_LEARN = "general"
 
 DEFAULT_OUT_DIR = "out"
 
+PRINT_EVERY_NUM_OF_EPOCHS = 100
+
 
 NNI = False
 
@@ -287,7 +289,7 @@ class Model:
             l1_norm += p.abs().sum()
         return l1_norm
 
-    def evaluate(self, evaluation_set: str = "test"):
+    def evaluate(self, evaluation_set: str = "test", evaluate_accuracy: bool = False):
         # self._gcn_rnn_net.eval()
 
         if evaluation_set == "test":
@@ -316,6 +318,16 @@ class Model:
             evaluation_set_current_labels = self.train_current_labels
             evaluation_set_next_labels = self.train_next_time_labels
             # End trial with Yoram
+        # Default to test+validation
+        elif evaluation_set == "testtrain":
+            self._gcn_rnn_net.eval()
+            evaluation_set_idx = self.validation_idx + self.test_idx
+            evaluation_set_learned_labels = torch.cat(
+                (self.validation_labels_to_learn, self.test_labels_to_learn))
+            evaluation_set_current_labels = torch.cat(
+                (self.validation_current_labels, self.test_current_labels))
+            evaluation_set_next_labels = torch.cat(
+                (self.validation_next_time_labels, self.test_next_time_labels))
         else:
             print(f"Invalid evaluation set: {evaluation_set}")
             return
@@ -325,15 +337,20 @@ class Model:
 
         val_loss += self._l1_lambda * self._l1_norm()
 
-        accuracy = r2_score(
-            val_output.cpu().detach().numpy(),
-            evaluation_set_learned_labels.float().cpu().detach().numpy(),
-        )
+        if evaluate_accuracy:
+            accuracy = r2_score(
+                val_output.cpu().detach().numpy(),
+                evaluation_set_learned_labels.float().cpu().detach().numpy(),
+            )
 
-        tot_accuracy = r2_score(
-            (evaluation_set_current_labels + val_output).cpu().detach().numpy(),
-            evaluation_set_next_labels.float().cpu().detach().numpy()
-        )
+            tot_accuracy = r2_score(
+                (evaluation_set_current_labels +
+                 val_output).cpu().detach().numpy(),
+                evaluation_set_next_labels.float().cpu().detach().numpy()
+            )
+        else:
+            accuracy = None
+            tot_accuracy = None
 
         return val_loss, accuracy, tot_accuracy
 
@@ -358,49 +375,65 @@ class Model:
             self._gcn_rnn_net.train()
             self._optimizer.zero_grad()
 
-            # output = self._gcn_rnn_net(self._gcn_data_list, self.train_idx)
-            # loss = self._criterion(output, self.train_labels)
-            # train_accuracy = r2_score(output.cpu().detach().numpy(), self.train_labels.float().cpu().detach().numpy())
-            train_loss, train_accuracy, train_tot_accuracy = self.evaluate("train")
-            train_loss_list.append(train_loss.data.cpu().item())
-            train_accuracy_list.append(train_accuracy)
-            train_tot_accuracy_list.append(train_tot_accuracy)
+            train_loss, _, _ = self.evaluate(
+                "train")
+            # train_loss_list.append(train_loss)
+            # train_accuracy_list.append(train_accuracy)
+            # train_tot_accuracy_list.append(train_tot_accuracy)
             train_loss.backward()
             self._optimizer.step()
 
-            # Evaluate validation set
-            self._gcn_rnn_net.eval()
-            validation_loss, validation_accuracy, validation_tot_accuracy = self.evaluate(
-                "validation")
-            val_loss_list.append(validation_loss.data.cpu().item())
-            val_accuracy_list.append(validation_accuracy)
-            val_tot_accuracy_list.append(validation_tot_accuracy)
+            # Comment out to improve performance. Will run only every 100 epochs:
 
-            if epoch % 100 == 99:
+            # Evaluate validation set
+            # self._gcn_rnn_net.eval()
+            # validation_loss, validation_accuracy, validation_tot_accuracy = self.evaluate(
+            #     "validation")
+            # val_loss_list.append(validation_loss)
+            # val_accuracy_list.append(validation_accuracy)
+            # val_tot_accuracy_list.append(validation_tot_accuracy)
+
+            if epoch % PRINT_EVERY_NUM_OF_EPOCHS == PRINT_EVERY_NUM_OF_EPOCHS - 1:
+                self._gcn_rnn_net.eval()
+                train_loss, train_accuracy, train_tot_accuracy = self.evaluate(
+                    "train", evaluate_accuracy=True)
+                validation_loss, validation_accuracy, validation_tot_accuracy = self.evaluate(
+                    "validation", evaluate_accuracy=True)
                 print(
-                    f"epoch: {epoch + 1}, train loss: {train_loss_list[-1]:.5f}, validation loss:{val_loss_list[-1]:.5f}, "
-                    f"train accuracy: {train_accuracy_list[-1]:.5f}, validation accuracy: {val_accuracy_list[-1]:.5f} "
-                    f"train tot accuracy: {train_tot_accuracy_list[-1]:.5f}, validation tot accuracy: {val_tot_accuracy_list[-1]:.5f} "
+                    f"epoch: {epoch + 1}, train loss: {train_loss.data.cpu().item():.5f}, validation loss:{validation_loss.data.cpu().item():.5f}, "
+                    f"train accuracy: {train_accuracy:.5f}, validation accuracy: {validation_accuracy:.5f} "
+                    f"train tot accuracy: {train_tot_accuracy:.5f}, validation tot accuracy: {validation_tot_accuracy:.5f} "
                 )
-        self._gcn_rnn_net.eval()
-        train_loss, train_accuracy, train_tot_accuracy = self.evaluate(
-            "train")
-        if NNI:
-            nni.report_intermediate_result(train_accuracy)
         return
 
-    def evaluate_zero_model_total_num(self, labels):
-        all_indices = set()
-        all_indices = all_indices.union(self.train_idx).union(
-            self.test_idx).union(self.validation_idx)
-        labels = torch.stack(
-            list(map(lambda x: self._get_labels_by_indices(x, all_indices), labels)))
+    def _get_stacked_labels_by_indices_set(self, labels, indices_set):
+        if indices_set == "test":
+            all_indices = self.test_idx
+        elif indices_set == "validation":
+            all_indices = self.validation_idx
+        elif indices_set == "train":
+            all_indices = self.train_idx
+        elif indices_set == "testtrain":
+            all_indices = set()
+            all_indices = all_indices.union(
+                self.test_idx).union(self.validation_idx)
+        else:
+            all_indices = set()
+            all_indices = all_indices.union(self.train_idx).union(
+                self.test_idx).union(self.validation_idx)
+        stacked_labels = torch.stack(
+            list(map(lambda x: self._get_labels_by_indices(x, self.test_idx), labels)))
+        return stacked_labels
+
+    def evaluate_zero_model_total_num(self, labels, indices):
+        stacked_labels = self._get_stacked_labels_by_indices_set(
+            labels, indices)
         losses = []
         predictions = []
         true_labels = []
         accuracies = []
-        for idx, l in enumerate(labels[:-1]):
-            true_labels.append(labels[idx + 1])
+        for idx, l in enumerate(stacked_labels[:-1]):
+            true_labels.append(stacked_labels[idx + 1])
             predictions.append(l)
 
             losses.append(self._criterion(predictions[-1], true_labels[-1]))
@@ -408,18 +441,15 @@ class Model:
                 predictions[-1].cpu().detach().numpy(), true_labels[-1].cpu().detach().numpy()))
         return losses, accuracies
 
-    def evaluate_zero_model_diff(self, labels):
-        all_indices = set()
-        all_indices = all_indices.union(self.train_idx).union(
-            self.test_idx).union(self.validation_idx)
-        labels = torch.stack(
-            list(map(lambda x: self._get_labels_by_indices(x, all_indices), labels)))
+    def evaluate_zero_model_diff(self, labels, indices):
+        stacked_labels = self._get_stacked_labels_by_indices_set(
+            labels, indices)
         losses = []
         predictions = []
         true_labels = []
         accuracies = []
-        for idx, l in enumerate(labels[:-1]):
-            true_labels.append(labels[idx + 1]-l)
+        for idx, l in enumerate(stacked_labels[:-1]):
+            true_labels.append(stacked_labels[idx + 1]-l)
             predictions.append(torch.zeros(len(l), device=DEVICE))
 
             losses.append(self._criterion(predictions[-1], true_labels[-1]))
@@ -427,24 +457,21 @@ class Model:
                 predictions[-1].cpu().detach().numpy(), true_labels[-1].cpu().detach().numpy()))
         return losses, accuracies
 
-    def evaluate_first_order_model_total_number(self, labels):
-        all_indices = set()
-        all_indices = all_indices.union(self.train_idx).union(
-            self.test_idx).union(self.validation_idx)
-        labels = torch.stack(
-            list(map(lambda x: self._get_labels_by_indices(x, all_indices), labels)))
+    def evaluate_first_order_model_total_number(self, labels, indices):
+        stacked_labels = self._get_stacked_labels_by_indices_set(
+            labels, indices)
         losses = []
         predictions = []
         true_labels = []
         accuracies = []
         lin_reg_models = []
-        time_steps = np.arange(len(labels)-1).reshape(-1, 1)
-        for idx, n in enumerate(labels.T):
+        time_steps = np.arange(len(stacked_labels)-1).reshape(-1, 1)
+        for idx, n in enumerate(stacked_labels.T):
             lin_reg_models.append(
                 LinearRegression().fit(time_steps, n[:-1].cpu()))
 
-        for idx, l in enumerate(labels[:-1]):
-            true_labels.append(labels[idx + 1])
+        for idx, l in enumerate(stacked_labels[:-1]):
+            true_labels.append(stacked_labels[idx + 1])
             predictions.append(torch.tensor([m.predict(
                 np.array([[idx+1]])) for m in lin_reg_models], device=DEVICE).view(-1))
 
@@ -454,27 +481,24 @@ class Model:
 
         return losses, accuracies
 
-    def evaluate_first_order_model_diff(self, labels):
-        all_indices = set()
-        all_indices = all_indices.union(self.train_idx).union(
-            self.test_idx).union(self.validation_idx)
-        labels = torch.stack(
-            list(map(lambda x: self._get_labels_by_indices(x, all_indices), labels)))
+    def evaluate_first_order_model_diff(self, labels, indices):
+        stacked_labels = self._get_stacked_labels_by_indices_set(
+            labels, indices)
         losses = []
         predictions = []
         true_labels = []
         accuracies = []
         lin_reg_models = []
-        time_steps = np.arange(len(labels)-1).reshape(-1, 1)
-        for idx, n in enumerate(labels.T):
+        time_steps = np.arange(len(stacked_labels)-1).reshape(-1, 1)
+        for idx, n in enumerate(stacked_labels.T):
             diffs = []
             for idx, m in enumerate(n[:-1]):
                 diffs.append(n[idx+1]-m)
             lin_reg_models.append(
                 LinearRegression().fit(time_steps, diffs))
 
-        for idx, l in enumerate(labels[:-1]):
-            true_labels.append(labels[idx + 1]-l)
+        for idx, l in enumerate(stacked_labels[:-1]):
+            true_labels.append(stacked_labels[idx + 1]-l)
             predictions.append(torch.tensor([m.predict(
                 np.array([[idx+1]])) for m in lin_reg_models], device=DEVICE).view(-1))
 
@@ -485,7 +509,7 @@ class Model:
         return losses, accuracies
 
 
-def train_test_split(graphs):
+def train_test_split(graphs, train_ratio):
     prev_graph = graphs[0]
     all_intersection = np.array(prev_graph.nodes())
     for g in graphs[1:]:
@@ -494,7 +518,7 @@ def train_test_split(graphs):
 
     print("# of nodes that appear at all timestamps", len(all_intersection))
     val_test_inds = np.random.choice(
-        all_intersection, round(len(all_intersection) * 0.2), replace=False
+        all_intersection, round(len(all_intersection) * (1-train_ratio)), replace=False
     )
     test, validation = np.array_split(val_test_inds, 2)
     train = np.setdiff1d(np.array(all_intersection), val_test_inds)
@@ -536,77 +560,76 @@ def run_trial(parameters):
     print(parameters)
     learned_label = parameters["learned_label"]
     graphs, labels, feature_mx, adjacency_matrices = load_input(parameters)
-    train, test, validation = train_test_split(graphs)
+    train, test, validation = train_test_split(graphs, train_ratio=0.7)
 
-    model_loss = []
-    model_accuracy = []
-    model_tot_accuracy = []
+    model_loss_list = []
+    model_accuracy_list = []
+    model_tot_accuracy_list = []
+    zero_model_tot_loss_list = []
+    zero_model_tot_accuracy_list = []
+    first_order_tot_loss_list = []
+    first_order_tot_accuracy_list = []
+    zero_model_diff_loss_list = []
+    zero_model_diff_accuracy_list = []
+    first_order_diff_loss_list = []
+    first_order_diff_accuracy_list = []
 
     model = Model(parameters)
     model.load_data(
-        graphs[:1],
+        graphs,
         feature_mx,
-        labels[0],
-        labels[1],
+        labels[-2],
+        labels[-1],
         learned_label,
         train,
         test,
         validation,
     )
+    model.train()
+    test_loss, test_accuracy, test_tot_accuracy = model.evaluate(
+        "testtrain", evaluate_accuracy=True)
 
-    for idx, g in enumerate(graphs[:-1]):
-        model = Model(parameters)
-        model.load_data(
-            [g],
-            feature_mx,
-            labels[idx],
-            labels[idx + 1],
-            learned_label,
-            train,
-            test,
-            validation,
-        )
-        model.train()
+    zero_model_tot_loss_list, zero_model_tot_accuracy_list = model.evaluate_zero_model_total_num(
+        labels, "testtrain")
+    first_order_tot_loss_list, first_order_tot_accuracy_list = model.evaluate_first_order_model_total_number(
+        labels, "testtrain")
+    zero_model_diff_loss_list, zero_model_diff_accuracy_list = model.evaluate_zero_model_diff(
+        labels, "testtrain")
+    first_order_diff_loss_list, first_order_diff_accuracy_list = model.evaluate_first_order_model_diff(
+        labels, "testtrain")
 
-        test_loss, test_accuracy, test_tot_accuracy = model.evaluate()
+    if NNI:
+        nni.report_intermediate_result(
+            test_tot_accuracy - zero_model_tot_accuracy_list[-1])
 
-        model_loss.append(test_loss)
-        model_accuracy.append(test_accuracy)
-        model_tot_accuracy.append(test_tot_accuracy)
-
-    zero_model_tot_loss, zero_model_tot_accuracy = model.evaluate_zero_model_total_num(
-        labels)
-    first_order_tot_loss, first_order_tot_accuracy = model.evaluate_first_order_model_total_number(
-        labels)
-    zero_model_diff_loss, zero_model_diff_accuracy = model.evaluate_zero_model_diff(
-        labels)
-    first_order_diff_loss, first_order_diff_accuracy = model.evaluate_first_order_model_diff(
-        labels)
+    model_loss_list.append(test_loss)
+    model_accuracy_list.append(test_accuracy)
+    model_tot_accuracy_list.append(test_tot_accuracy)
 
     print(
-        f"test loss: {np.mean([m.data.item() for m in model_loss]):.5f}, test_tot_accuracy: {np.mean(model_tot_accuracy):.5f}, test_diff_accuracy: {np.mean(model_accuracy):.5f}")
+        f"test loss: {np.mean([m.data.item() for m in model_loss_list]):.5f}, test_tot_accuracy: {np.mean(model_tot_accuracy_list):.5f}, test_diff_accuracy: {np.mean(model_accuracy_list):.5f}")
     print(
-        f"zero model loss: {np.mean([m.data.item() for m in zero_model_tot_loss]):.5f}, zero model accuracy: {np.mean(zero_model_tot_accuracy):.5f}")
+        f"zero model loss: {np.mean([m.data.item() for m in zero_model_tot_loss_list]):.5f}, zero model accuracy: {np.mean(zero_model_tot_accuracy_list):.5f}")
     print(
-        f"first order model loss: {np.mean([m.data.item() for m in first_order_tot_loss]):.5f}, first order model accuracy: {np.mean(first_order_tot_accuracy):.5f}")
+        f"first order model loss: {np.mean([m.data.item() for m in first_order_tot_loss_list]):.5f}, first order model accuracy: {np.mean(first_order_tot_accuracy_list):.5f}")
     print(
-        f"zero diff model loss: {np.mean([m.data.item() for m in zero_model_diff_loss]):.5f}, zero diff model accuracy: {np.mean(zero_model_diff_accuracy):.5f}")
+        f"zero diff model loss: {np.mean([m.data.item() for m in zero_model_diff_loss_list]):.5f}, zero diff model accuracy: {np.mean(zero_model_diff_accuracy_list):.5f}")
     print(
-        f"first order diff model loss: {np.mean([m.data.item() for m in first_order_diff_loss]):.5f}, first order diff model accuracy: {np.mean(first_order_diff_accuracy):.5f}")
+        f"first order diff model loss: {np.mean([m.data.item() for m in first_order_diff_loss_list]):.5f}, first order diff model accuracy: {np.mean(first_order_diff_accuracy_list):.5f}")
     print("\n")
 
     return (
-        [m.data.item() for m in model_loss],
-        model_accuracy,
-        model_tot_accuracy,
-        [m.data.item() for m in zero_model_tot_loss],
-        zero_model_tot_accuracy,
-        [m.data.item() for m in first_order_tot_loss],
-        first_order_tot_accuracy,
-        [m.data.item() for m in zero_model_diff_loss],
-        zero_model_diff_accuracy,
-        [m.data.item() for m in first_order_diff_loss],
-        first_order_diff_accuracy
+        [m.data.item() for m in model_loss_list],
+        model_accuracy_list,
+        model_tot_accuracy_list,
+        [m.data.item() for m in zero_model_tot_loss_list],
+        zero_model_tot_accuracy_list,
+        [m.data.item() for m in first_order_tot_loss_list],
+        first_order_tot_accuracy_list,
+        [m.data.item() for m in zero_model_diff_loss_list],
+        zero_model_diff_accuracy_list,
+        [m.data.item() for m in first_order_diff_loss_list],
+        first_order_diff_accuracy_list
     )
 
 
@@ -614,15 +637,15 @@ if __name__ == "__main__":
     _params = {
         "data_name": "dnc",
         "net": GCNNet,
-        "l1_lambda": 0,
-        "epochs": 1000,
-        "gcn_dropout_rate": 0.5,
+        "l1_lambda": 7.80299277019119E-07,
+        "epochs": 500,
+        "gcn_dropout_rate": 0.805860891339941,
         "lstm_dropout_rate": 0,
-        "gcn_hidden_sizes": [40, 40],
-        "learning_rate": 0.0005,
-        "weight_decay": 0.00003,
+        "gcn_hidden_sizes": [100, 100],
+        "learning_rate": 0.0000599683618989071,
+        "weight_decay": 0.00343940903913481,
         "gcn_latent_dim": 10,
-        "lstm_hidden_size": 10,
+        "lstm_hidden_size": 50,
         "lstm_num_layers": 1,
         "learned_label": DEFAULT_LABEL_TO_LEARN,
     }
@@ -663,5 +686,9 @@ if __name__ == "__main__":
     ) = run_trial(_params)
 
     if NNI:
-        nni.report_final_result(np.mean(model_tot_accuracy))
-
+        nni.report_final_result(
+            model_tot_accuracy[0]-zero_model_tot_accuracy[-1])
+    else:
+        print(f"Final result: model tot accuracy: {model_tot_accuracy}, zero_model_tot_accuracy: {zero_model_tot_accuracy}, "
+        f"first order tot accuracy: {first_order_tot_accuracy}, zero model diff accuracy: {zero_model_diff_accuracy}, "
+        f"first order diff accuracy: {first_order_diff_accuracy}")
