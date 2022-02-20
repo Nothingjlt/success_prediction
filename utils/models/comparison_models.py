Fix R2 calculation bug minor fixes

utils/models/comparison_models.py
@@ -228,7 +228,7 @@ class PeriodicAveragePredictor(AveragePredictor):
         self._epsilon = epsilon
         self._d_period = [
             i % self._period for i in range(1, self._average_time + 1)
-        ]
+        ] # TODO fix bug as this needs to be (reversed([i $ self._period for i in range(1, self._average_time + 1)]))
 
 
 # As defined in https://www.cl.cam.ac.uk/~rja14/Papers/computernets12.pdf under 5.1 Periodic Intervals
@@ -276,6 +276,38 @@ class WeightedPeriodicAverageModel(PeriodicAveragePredictor):
                     )
                 ) * d
             )
-            for d_period, d in zip(self._d_period, range(1, self._average_time + 1))
+            for d_period, d in zip(self._d_period, range(1, self._average_time + 1)) # TODO fix bug where d need to be reversed(range(1, self._average_time + 1))
         ]
         self._total_weight = np.sum(self._weights)
+
+
+# As defined in https://link.springer.com/content/pdf/10.1007/s00779-016-0958-0.pdf under 5.2 Periodical Average Method
+# in our case m = average_time, p = period, and w is normalized to be 1
+class ZhouPeriodicAveragePredictor(AveragePredictor):
+    def __init__(self, average_time, period):
+        super(ZhouPeriodicAveragePredictor, self).__init__(
+            average_time
+        )
+        self._period = period
+        self._a = np.min([x % period for x in range(self._average_time, 0, -1)])
+        self._fs = [1 if k % self._period == self._a else 0 for k in range(self._average_time + 1, 1, -1)]
+
+
+class ZhouPeriodicAverageModel(ZhouPeriodicAveragePredictor):
+    def __init__(self, average_time, period):
+        super(ZhouPeriodicAverageModel, self).__init__(
+            average_time,
+            period
+        )
+        self._weights = self._fs
+
+
+class ZhouWeightedPeriodingAverageModel(ZhouPeriodicAveragePredictor):
+    def __init__(self, average_time, period):
+        super(ZhouPeriodicAveragePredictor, self).__init__(
+            average_time,
+            period
+        )
+        self._ds = [(self._average_time - k)/self._period for k in range(average_time + 1, 1, -1)]
+        self._ws = map(lambda x: 1/x, self._ds)
+        self._weights = np.multiply(self._ws, self._fs)

utils/utils.py
@@ -1,13 +1,9 @@
-from numpy.lib.function_base import average
 import torch
-from sklearn.metrics import mean_squared_error
-from torch.nn.modules import loss
 from torch_geometric.data import Data
 from torch_geometric.utils import add_self_loops
 import numpy as np
-from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from scipy.stats import pearsonr, spearmanr
-from sklearn.linear_model import LinearRegression
 from utils.models import comparison_models
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -225,15 +221,15 @@ class GraphSeriesData():
 
     def _calc_criterion_np(self, predictions_np, true_labels_np, criterion, loss_criterion=mean_squared_error):
         if criterion == 'r2_score':
-            return r2_score(predictions_np, true_labels_np)
+            return r2_score(true_labels_np, predictions_np)
         if criterion == 'correlation':
-            return spearmanr(predictions_np, true_labels_np).correlation
+            return spearmanr(true_labels_np, predictions_np).correlation
         if criterion == 'mae':
-            return mean_absolute_error(predictions_np, true_labels_np)
+            return mean_absolute_error(true_labels_np, predictions_np)
         if criterion == 'mse':
-            return mean_squared_error(predictions_np, true_labels_np)
+            return mean_squared_error(true_labels_np, predictions_np)
         if criterion == 'loss':
-            return loss_criterion(predictions_np, true_labels_np)
+            return loss_criterion(true_labels_np, predictions_np)
 
     def _prepare_and_calc_criterion(self, predictions, true_labels, criterion):
         if self._learn_logs: